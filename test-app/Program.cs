4c5612a48cc53b5bbef6461b76b6cd86da4d5235
Peyton Krimmel <pkrimmel0@gmail.com>
2022-03-04 20:19:19 -0500

    my first commit

---
+++ test-app/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Console.WriteLine("enter some text");
+// var myVar = Console.ReadLine();
+// var myVar = "asdf";
+// Console.WriteLine("the text was " + myVar);
+
+//var rnd = new Random();
+
+class MyRandom
+{
+    public static void Main()
+    {
+        var rnd = new Random();
+        Console.WriteLine("enter the 1st number");
+        var NumOne = int.Parse(Console.ReadLine());
+        Console.WriteLine("enter the 2nd number");
+        var NumTwo = int.Parse(Console.ReadLine());
+        Console.WriteLine("a number between " + NumOne + " and " + NumTwo + " is: ");
+        Console.WriteLine(rnd.Next(NumOne, NumTwo));
+
+    }
+}
+
+
+/*
+class AwesomeApp
+{
+    public static void Main()
+    {
+        Console.WriteLine("notice me senpai");
+    }
+    public AwesomeApp()
+    {
+
+    }
+}
+*/
+
